68caed8a2fb17d9a856259f9d404f36a5dd9b934
Sergei Samoilov <samoilovsergey84@gmail.com>
2023-02-23 19:48:31 +0300

    Deleted additinal empty line

--- src/main/java/com/ourcampany/farmapp/FarmAppApplication.java
+++ src/main/java/com/ourcampany/farmapp/FarmAppApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FarmAppApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(FarmAppApplication.class, args);
 	}
