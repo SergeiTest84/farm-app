230cfe2c68db6ef43208f5b9dfcf1c22d7942b53
Sergei Samoilov <samoilovsergey84@gmail.com>
2023-02-23 19:45:36 +0300

    Added additinal empty line

--- src/main/java/com/ourcampany/farmapp/FarmAppApplication.java
+++ src/main/java/com/ourcampany/farmapp/FarmAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FarmAppApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(FarmAppApplication.class, args);
 	}
